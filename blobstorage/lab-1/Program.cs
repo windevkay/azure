0335192c443f036c542b99ba3ba6f0d6015581bc
kolaadebayo <kolabayo360@gmail.com>
2023-09-06 18:09:50 -0600

    key removal

--- blobstorage/lab-1/Program.cs
+++ blobstorage/lab-1/Program.cs
@@ -12,7 +12,7 @@ Console.ReadLine();
 static async Task ProcessAsync()
 {
     // Copy the connection string from the portal in the variable below.
-    string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=windevlabstorage;AccountKey=V5oU3BS9+fjaWdvoL9frtWFtO0SWCA5+xzwNmedn+LCytMWbnfSQisXSqIHYL0SwZ896Eq1mvTgy+AStfvsoEA==;EndpointSuffix=core.windows.net";
+    string storageConnectionString = "";
 
     // Create a client that can authenticate with a connection string
     BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
